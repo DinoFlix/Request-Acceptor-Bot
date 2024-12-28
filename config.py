4a4f7d4ac5f11ea98390366eeaab69e1767b1d07
DinoFlix <dinoflix44@gmail.com>
2024-12-28 11:52:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
+API_ID = int(environ.get("API_ID", "21335208"))
+API_HASH = environ.get("API_HASH", "4cb201499c03fe54f817617d27b336aa")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7709792728:AAGfZ0LpwDM3FAy1THeTUrP6Je-rWlGkrNE")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002354059393"))
+ADMINS = int(environ.get("ADMINS", "1577166444"))
+DB_URI = environ.get("DB_URI", "mongodb+srv://thomusoro9:thomus@cluster0.molki.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = environ.get("DB_NAME", "thomusoro9")
